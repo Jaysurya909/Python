c9a59220ff0898786a2746ce560cde344629cba7
JaySurya <jaysurya6969@gmail.com>
2025-06-26 10:10:22 +0530

    for loop using else statement

---
+++ elsewithforloop.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    print(i)
+    if i == 7:
+        break
+
+else:
+    print("Successful print")
+
+
+print("Another for loop with successful print\n")
+
+for i in range(10):
+    print(i)
+
+else:
+    print("Successful loop")
