6de27934e87442753f520984ec4cf3ba7a226aee
JaySurya <jaysurya6969@gmail.com>
2025-07-10 09:16:58 +0530

    starting with classes

---
+++ classes.py
@@ -0,0 +1,26 @@
+class person:
+    id=1
+    name="BOT"
+    salary=10000
+    def info(me):
+        print(f"{me.id} is id of {me.name} and his salary is {me.salary}")
+
+
+
+jay=person()
+
+jay.id=1560
+jay.name="Jaysurya909"
+jay.salary=50000000
+
+# print(jay.id,jay.name,jay.salary)
+
+jay.info()
+
+noob=person()
+
+noob.info()
+
+
+
+
