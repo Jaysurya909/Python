d6ccce2f9dc0048b2505d221e05d45f53d10db38
JaySurya <jaysurya6969@gmail.com>
2025-07-20 11:25:52 +0530

    the mro concept of inheritance in python

---
+++ MRO_order_inpython.py
@@ -0,0 +1,15 @@
+class A:
+    def show(self): print("A")
+
+class B(A):
+    def show(self): print("B")
+
+class C(A):
+    def show(self): print("C")
+
+class D(B, C):
+    pass
+
+d = D()
+d.show() # it follows the path D → B → C → A → object and prints b in this case
+
