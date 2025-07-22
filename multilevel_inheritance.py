6b2e61751323cb4ddcc20dea7a76a8eab7b98761
JaySurya <jaysurya6969@gmail.com>
2025-07-22 09:53:45 +0530

    The hardcode method for Multilevel inheritance

---
+++ multilevel_inheritance.py
@@ -0,0 +1,35 @@
+# The hardcode method for Multilevel inheritance
+
+class Animal:
+    def __init__(self,Animal_name):
+        self.Animal_name=Animal_name
+
+    def showinfo(self):
+        print(f"The name of the animal is:- {self.Animal_name}")
+
+class Dog(Animal):
+    def __init__(self,breed):
+        Animal.__init__(self,Animal_name="Dog")
+        self.breed=breed
+    
+    def showinfo(self):
+        Animal.showinfo(self)
+        print(f"The breed of the dog is:- {self.breed}")
+
+class Bulldog(Dog):
+    def __init__(self, dog_name,color):
+        Dog.__init__(self,breed="Bulldog")
+        
+        self.dog_name=dog_name
+        self.color=color
+
+    def showinfo(self):
+        Dog.showinfo(self) # Also have to give self for first arguement here also
+        print(f"The name of the dog is:- {self.dog_name}\nAnd the color of the dog is:- {self.color}")
+        
+
+p1=Bulldog("Tommy","White")
+p1.showinfo()
+        
+
+        
