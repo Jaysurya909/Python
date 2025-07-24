f23c53f0d6549ff24fd9da07a765c4cc4795e0a8
JaySurya <jaysurya6969@gmail.com>
2025-07-24 14:44:51 +0530

    shutil model in python

---
+++ shutil_model.py
@@ -0,0 +1,6 @@
+import shutil
+# shutil.copy("walrus_operator.py","shutil_model.py") # will copy the contents of the first file to second file
+# shutil.move("C:\Jay\Python_MRO_and_Str_vs_Show_Chat.pdf","C:\Jay\PYTHON") # used to move a file from one folder to another
+# shutil.rmtree("backup")  # Used to delete a folder 
+
+# there are many other functions that shutil model can do
