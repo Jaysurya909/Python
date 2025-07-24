9bad6fe721e885de22464e5708d3e2af83648ead
JaySurya <jaysurya6969@gmail.com>
2025-07-24 14:24:21 +0530

    use of walrus operator (:=)

---
+++ walrus_operator.py
@@ -0,0 +1,9 @@
+# print(a=56) you cannot do this it will give error
+print(a:=56) # walrus operator will print 
+
+while (food:=input("Enter your favourite food")) != "quit":  # we used the walrus operator to take input 
+    print(food)
+
+
+
+# := this is walrus operator
